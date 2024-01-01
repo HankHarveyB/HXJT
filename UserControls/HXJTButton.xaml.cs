db065e56eaeff0ace8f1f7c2d8c85f7087fa15e4
HankHarveyB <hankharveyb@gmail.com>
2024-01-01 20:53:35 +0800

    使用自定义控件的形式实现了自定义学术活动的按钮

--- UserControls/HXJTButton.xaml.cs
+++ UserControls/HXJTButton.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HXJT.Views.UserControl;
+namespace HXJT.UserControls;
 /// <summary>
 /// HXJTButton.xaml 的交互逻辑
 /// </summary>

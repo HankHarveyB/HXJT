27fc638107c6a9b60266c79d83110281534da9b7
HankHarveyB <hankharveyb@gmail.com>
2024-01-01 20:23:19 +0800

    第二次测试加上强制执行

--- ViewModels/Pages/HXJTViewModel.cs
+++ ViewModels/Pages/HXJTViewModel.cs
@@ -33,7 +33,7 @@ public partial class HXJTViewModel : ObservableObject
                 from activity in ActivitiesCollection
                 where activity.AcademicName.StartsWith("“虹”学讲堂")
                 orderby activity.AcademicStarttime descending
-                select activity);
+                select activity).ToList();
 
 
             

4c7698a8ce07759d11a3324293bc0f9ca4a65089
HankHarveyB <hankharveyb@gmail.com>
2024-01-02 10:41:42 +0800

    实现了提示功能

--- UserControls/HXJTButton.xaml.cs
+++ UserControls/HXJTButton.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HXJT.Models;
+using Wpf.Ui.Common;
+using Wpf.Ui.Controls;
 
 namespace HXJT.UserControls;
 /// <summary>
@@ -19,8 +22,24 @@ namespace HXJT.UserControls;
 /// </summary>
 public partial class HXJTButton : UserControl
 {
+    private readonly ISnackbarService _snackbarService;
     public HXJTButton()
     {
         InitializeComponent();
+        this._snackbarService = App.GetService<ISnackbarService>();
+    }
+    private void ShowResult(string title,string info)
+    {
+        _snackbarService.Show(
+            title,
+            info,
+            ControlAppearance.Secondary,
+            new SymbolIcon(SymbolRegular.Fluent24),
+            TimeSpan.FromSeconds(2)
+        );
+    }
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        ShowResult("抢票结果",(DataContext as AcademicActivity).AcademicName);
     }
 }

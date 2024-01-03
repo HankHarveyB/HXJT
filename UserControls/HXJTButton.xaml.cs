0e6b4704cb331e79776182f9f7264074e2d8a321
HankHarveyB <hankharveyb@gmail.com>
2024-01-03 17:26:42 +0800

    修改了按钮点击的命令，使得界面更加自然

--- UserControls/HXJTButton.xaml.cs
+++ UserControls/HXJTButton.xaml.cs
@@ -27,61 +27,8 @@ namespace HXJT.UserControls;
 /// </summary>
 public partial class HXJTButton : UserControl
 {
-    //private readonly ISnackbarService _snackbarService;
-    //public HXJTButtonViewModel ViewModel
-    //{
-    //    get;
-    //}
     public HXJTButton()
     {
         InitializeComponent();
-        //this._snackbarService = App.GetService<ISnackbarService>();
     }
-    /// <summary>
-    /// 弹窗提示结果
-    /// </summary>
-    /// <param name="title"></param>
-    /// <param name="info"></param>
-    //private void ShowResult(string title,string info,Boolean IsSuccess)
-    //{
-    //    var appearance = IsSuccess ? ControlAppearance.Success : ControlAppearance.Danger;
-    //    Dispatcher.InvokeAsync(
-    //        () => {
-    //                    _snackbarService.Show(
-    //                    title,
-    //                    info,
-    //                    appearance,
-    //                    new SymbolIcon(SymbolRegular.Fluent24),
-    //                    TimeSpan.FromSeconds(0.5)
-    //                    );
-
-    //              }
-    //        );
-        
-    //}
-    
-    //private async void Button_Click(object sender, RoutedEventArgs e)
-    //{
-    //    for (var i = 0; i <= 3; i++)
-    //    {
-            
-    //    var result = await HTTPHelper.AddTicket((this.DataContext as AcademicActivity)!.Id);
-    //    if (result.Contains("已报名") || result.Contains("成功"))
-    //    {
-    //    ShowResult(result,"",true);
-    //    }
-    //    else
-    //    {
-    //        ShowResult(result, "", false);
-    //    }
-    //    }
-
-        // Task.Run(async () =>
-        //{
-        //    var result = await HTTPHelper.AddTicket((this.DataContext as AcademicActivity)!.Id)!;
-        //    ShowResult("result", result);
-
-        //});
-
-    //}
 }
--- ViewModels/UserControls/HXJTButtonViewModel.cs
+++ ViewModels/UserControls/HXJTButtonViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Threading;
-using HXJT.Helpers;
+﻿using HXJT.Helpers;
 using HXJT.Models;
 using Wpf.Ui.Common;
 using Wpf.Ui.Controls;
@@ -12,50 +6,60 @@ using Wpf.Ui.Controls;
 namespace HXJT.ViewModels.UserControls;
 
 public partial class HXJTButtonViewModel : ObservableObject
+{
+    private readonly ISnackbarService _snackbarService;
+    [ObservableProperty]
+    private AcademicActivity academicActivity;
+    public HXJTButtonViewModel(AcademicActivity academicActivity)
     {
-        private readonly ISnackbarService _snackbarService;
-        [ObservableProperty]
-        private AcademicActivity academicActivity;
-        public HXJTButtonViewModel(AcademicActivity academicActivity)
-        {
-            _snackbarService = App.GetService<ISnackbarService>();
-            this.academicActivity = academicActivity;
-        }
-        private void ShowResult(string title, string info, Boolean IsSuccess)
-        {
-            var appearance = IsSuccess ? ControlAppearance.Success : ControlAppearance.Danger;
-
-            _snackbarService.Show(
-            title,
-            info,
-            appearance,
-            new SymbolIcon(SymbolRegular.Fluent24),
-            TimeSpan.FromSeconds(0.5)
-            );
-        }
-        [RelayCommand]
-        private async Task askForTicket()
-        {
-            for (var i = 0; i <= 3; i++)
-            {
-
-                var result = await HTTPHelper.AddTicket(this.AcademicActivity!.Id); ;
-                if (result.Contains("已报名") || result.Contains("成功"))
-                {
-                    ShowResult(result, "", true);
-                }
-                else
-                {
-                    ShowResult(result, "", false);
-                }
-            }
-
-            // Task.Run(async () =>
-            //{
-            //    var result = await HTTPHelper.AddTicket((this.DataContext as AcademicActivity)!.Id)!;
-            //    ShowResult("result", result);
-
-            //});
-        }
+        _snackbarService = App.GetService<ISnackbarService>();
+        this.academicActivity = academicActivity;
     }
+    private void ShowResult(string title, string info, Boolean IsSuccess)
+    {
+        var appearance = IsSuccess ? ControlAppearance.Success : ControlAppearance.Danger;
+
+        _snackbarService.Show(
+        title,
+        info,
+        appearance,
+        new SymbolIcon(SymbolRegular.Fluent24),
+        TimeSpan.FromSeconds(2)
+        );
+    }
+    [RelayCommand]
+    private async Task askForTicket()
+    {
+        var isSuccess = false;
+        var result="";
+        //ShowResult("正在抢票···", "", true);
+        int i;
+        for (i = 1; i <= 3; i++)
+        {
+
+            result = await HTTPHelper.AddTicket(this.AcademicActivity!.Id); ;
+            if (result.Contains("已报名") || result.Contains("成功"))
+            {
+                isSuccess = true;
+                break;
+            }
+        }
+        if (isSuccess)
+        {
+            ShowResult(result!, "共发起了" + i + "次请求", true);
+        }
+        else
+        {
+            ShowResult(result!, "共发起了" + i + "次请求", false);
+        }
+
+
+        // Task.Run(async () =>
+        //{
+        //    var result = await HTTPHelper.AddTicket((this.DataContext as AcademicActivity)!.Id)!;
+        //    ShowResult("result", result);
+
+        //});
+    }
+}
 
